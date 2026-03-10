9dfd5b9d6ac8e4a0d6860f614d0ca9cd556e3f38
Frankrijkk <franciszek2005@gmail.com>
2026-03-10 09:28:00 +0100

    move everything into Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+Console.WriteLine("salamalejkum");
+Console.WriteLine("Shabat Shalom");
+
+
